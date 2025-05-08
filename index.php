cfefab02465c3d5cd81aae5c99f0bcbabae0e6da
JOAO VITOR <joaovitorgoncalvessantos69@gmail.com>
2025-05-08 19:22:20 -0300

    cass e htlm
    fiz o hhtml e o css,e agr estou transformando em um site com php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cartas de procurado</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <section class="container">
+        <h1 class="title">Cartas de Procurado</h1>
+
+        <div class="cartas">
+            <div class="procurado">
+                <h2>Procurado</h2>
+            </div>
+            <div class="img">
+                <img src="img/luffy.png" alt="Imagem do Luffy" class="img-luffy">
+            </div>
+            <div class="dead-or-alive">
+                <h2>Dead or Alive</h2>
+                <h2 class="nome-personagem">Luffy</h2>
+                <h2 class="recompensa">1.500.000.000</h2>
+            </div>
+        </div>
+
+    </section>
+</body>
+</html>
